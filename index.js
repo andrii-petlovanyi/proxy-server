578d4211ed19fff5933caf7e50008a97f9cc9117
Andrii Petlovanyi <apetlovanyi@cloverdynamics.com>
2023-12-29 15:52:44 +0200

    delete: log

--- index.js
+++ index.js
@@ -5,12 +5,7 @@ const { backcountryMiddleware } = require("./targets/backcountry");
 const app = express();
 const PORT = 3000;
 
-const customMiddleware = (req, res, next) => {
-  console.log(console.log(req.url));
-  next();
-};
-
-app.use("/backcountry", customMiddleware, backcountryMiddleware);
+app.use("/backcountry", backcountryMiddleware);
 
 app.listen(PORT, () => {
   console.log(`Proxy server is running on http://localhost:${PORT}`);
