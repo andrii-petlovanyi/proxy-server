fe41f3d27b5005b0264e9e569bcecd187d39351c
Andrii Petlovanyi <apetlovanyi@cloverdynamics.com>
2023-12-31 21:13:08 +0200

    fixed: added user agent for macys target

--- targets/macys.js
+++ targets/macys.js
@@ -6,6 +6,12 @@ const macysMiddleware = createProxyMiddleware({
   pathRewrite: {
     "^/macys": "",
   },
+  onProxyReq: function (proxyReq) {
+    proxyReq.setHeader(
+      "User-Agent",
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+    );
+  },
 });
 
 module.exports = {
