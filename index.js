b5e67d923c6fa2d3999f31ecb14b00b9c7448c13
Andrii Petlovanyi <apetlovanyi@cloverdynamics.com>
2024-01-05 23:26:01 +0200

    added: target benetton

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const express = require("express");
 const { backcountryMiddleware } = require("./targets/backcountry");
 const { yooxMiddleware } = require("./targets/yoox");
 const { macysMiddleware } = require("./targets/macys");
+const { benettonMiddleware } = require("./targets/benetton");
 
 const app = express();
 const PORT = 3000;
 
 app.use("/backcountry", backcountryMiddleware);
+app.use("/benetton", benettonMiddleware);
 app.use("/yoox", yooxMiddleware);
 app.use("/macys", macysMiddleware);
 
