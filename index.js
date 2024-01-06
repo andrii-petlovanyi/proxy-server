744fc2cd626dd87b375e1897a6503f370ceffc01
Andrii Petlovanyi <apetlovanyi@cloverdynamics.com>
2024-01-06 20:13:07 +0200

    added: moosejaw target

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require("express");
-
 const { backcountryMiddleware } = require("./targets/backcountry");
+const { benettonMiddleware } = require("./targets/benetton");
+const { moosejawMiddleware } = require("./targets/moosejaw");
 const { yooxMiddleware } = require("./targets/yoox");
 const { macysMiddleware } = require("./targets/macys");
-const { benettonMiddleware } = require("./targets/benetton");
 
 const app = express();
 const PORT = 3000;
 
 app.use("/backcountry", backcountryMiddleware);
 app.use("/benetton", benettonMiddleware);
+app.use("/moosejaw", moosejawMiddleware);
 app.use("/yoox", yooxMiddleware);
 app.use("/macys", macysMiddleware);
 
